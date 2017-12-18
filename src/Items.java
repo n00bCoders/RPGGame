4f6d68fb86780c5d2f968e33a040f8c4a56d6166
Patryk <patryk.golebiowski1@outlook.com>
2017-12-18 20:32:27 +0100

    small changes

--- src/Items.java
+++ src/Items.java
@@ -1,6 +1,12 @@
 public class Items{
 
+    static int id;
+    static String name;
 
+    public Items(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
 
 }
 
--- src/Weapons.java
+++ src/Weapons.java
@@ -1,5 +1,7 @@
 public class Weapons extends Items{
 
-    
+    public Weapons(int id, String name, double atk, double str, double crit){
+        super(id, name);
+    }
 
 }
